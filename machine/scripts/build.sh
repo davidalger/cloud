a28b0f7fb126502fa1a404415e5bd1d5a473f662
David Alger <david@classyllama.com>
2016-01-15 19:20:02 -0600

    Fixed bug in how bin dir contents were being loaded; new nodes created from manager would fail

--- machine/scripts/build.sh
+++ machine/scripts/build.sh
@@ -15,9 +15,10 @@ if [[ ! -d $REMOTE_BASE/vendor ]]; then
 fi
 
 # pull vendor assets into place, then cleanup
+mkdir -p $VAGRANT_DIR/bin/
 rsync -a --ignore-existing $REMOTE_BASE/vendor/davidalger/devenv/vagrant/{etc,scripts} $VAGRANT_DIR/
 rsync -a --ignore-existing $REMOTE_BASE/vendor/davidalger/devenv/vagrant/lib/ $REMOTE_BASE/lib/
 cp $REMOTE_BASE/vendor/davidalger/devenv/vagrant/scripts/bootstrap.sh $VAGRANT_DIR/
-cp $REMOTE_BASE/vendor/davidalger/devenv/vagrant/bin/magento /usr/local/bin/
-cp $REMOTE_BASE/vendor/davidalger/devenv/vagrant/bin/m2setup.sh /usr/local/bin/
+cp $REMOTE_BASE/vendor/davidalger/devenv/vagrant/bin/magento $VAGRANT_DIR/bin/
+cp $REMOTE_BASE/vendor/davidalger/devenv/vagrant/bin/m2setup.sh $VAGRANT_DIR/bin/
 rm -rf $REMOTE_BASE/vendor
