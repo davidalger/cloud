55da1ecc47d06f295d3adc7ae63217086b1dd88e
David Alger <david@classyllama.com>
2016-01-15 20:25:24 -0600

    Added ability to sync composer data into demo host

--- etc/conf.d/demo.rb
+++ etc/conf.d/demo.rb
@@ -1,4 +1,5 @@
 conf.vm.define :demo do |node|
   machine_fullstack_vm node, host: 'demo'
+  conf.vm.synced_folder BASE_DIR + '/etc/data.d/demo/etc/composer', REMOTE_BASE + '/etc/composer', type: 'rsync'
   mage2_install node, host: 'demo', db_name: 'mage2_demo'
 end
