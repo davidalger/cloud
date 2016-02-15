1e33a04255eeb4c8c4051f3e0e3811cb76a9263a
David Alger <david@classyllama.com>
2016-02-15 14:02:25 -0600

    Fixed bug in not having the actual source for sites.rb IN the repo :P

---
+++ etc/conf.d/sites.rb
@@ -0,0 +1,5 @@
+Dir.foreach BASE_DIR + '/etc/sites.d' do | file |
+  if not File.directory? BASE_DIR + '/etc/sites.d/' + file and file =~ /.*\.rb$/
+    include_conf BASE_DIR + '/etc/sites.d/' + file, conf
+  end
+end
