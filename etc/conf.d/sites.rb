7def5bb9538908c296ed925d0bf44d752bae1d5c
David Alger <david@classyllama.com>
2016-04-29 11:52:52 -0500

    Fixed vagrant error triggered when running vagrant status before deploying any custom sites.d configuration files

--- etc/conf.d/sites.rb
+++ etc/conf.d/sites.rb
@@ -1,5 +1,7 @@
-Dir.foreach BASE_DIR + '/etc/sites.d' do | file |
-  if not File.directory? BASE_DIR + '/etc/sites.d/' + file and file =~ /.*\.rb$/
-    include_conf BASE_DIR + '/etc/sites.d/' + file, conf
+if File.directory? BASE_DIR + '/etc/sites.d'
+  Dir.foreach BASE_DIR + '/etc/sites.d' do | file |
+    if not File.directory? BASE_DIR + '/etc/sites.d/' + file and file =~ /.*\.rb$/
+      include_conf BASE_DIR + '/etc/sites.d/' + file, conf
+    end
   end
 end
