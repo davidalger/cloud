0a9aeb89fc672eed1d6b549184aa6270ef702a0e
David Alger <david@classyllama.com>
2016-04-29 16:20:01 -0500

    Updated sandbox to allow disabling it

--- etc/conf.d/sandbox.rb
+++ etc/conf.d/sandbox.rb
@@ -1,3 +1,6 @@
-conf.vm.define :sandbox do |node|
-  machine_fullstack_vm node, host: 'sandbox'
+if defined? SANDBOX_SITE and SANDBOX_SITE == true
+  node_name = 'sandbox'
+  conf.vm.define node_name do |node|
+    machine_fullstack_vm node, host: node_name
+  end
 end
