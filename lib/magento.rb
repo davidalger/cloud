1a00602c0322835e4be21a293cc8de38c8c12ab1
David Alger <david@classyllama.com>
2016-05-18 13:01:10 -0500

    Added skip-root-check flag to mage-run calls to avoid a bunch of msgs being output on DO nodes

--- lib/magento.rb
+++ lib/magento.rb
@@ -36,13 +36,13 @@ def install_magento2 (node, host: nil, path: nil, database: nil, enterprise: fal
       echo 'Initializing software configuration'
       
       cd $INSTALL_DIR
-      mr2 -q setup:config:set --no-interaction --http-cache-hosts=127.0.0.1:6081
-      mr2 -q config:set system/full_page_cache/caching_application 2
-      mr2 -q config:set system/full_page_cache/ttl 604800
-      mr2 -q config:set system/full_page_cache/varnish/access_list localhost
-      mr2 -q config:set system/full_page_cache/varnish/backend_host localhost
-      mr2 -q config:set system/full_page_cache/varnish/backend_port 8080
-      mr2 -q cache:flush
+      mr2 -q --skip-root-check setup:config:set --no-interaction --http-cache-hosts=127.0.0.1:6081
+      mr2 -q --skip-root-check config:set system/full_page_cache/caching_application 2
+      mr2 -q --skip-root-check config:set system/full_page_cache/ttl 604800
+      mr2 -q --skip-root-check config:set system/full_page_cache/varnish/access_list localhost
+      mr2 -q --skip-root-check config:set system/full_page_cache/varnish/backend_host localhost
+      mr2 -q --skip-root-check config:set system/full_page_cache/varnish/backend_port 8080
+      mr2 -q --skip-root-check cache:flush
       
       echo 'Setting file permissions and ownership'
       
@@ -79,19 +79,19 @@ def install_magento1 (node, host: nil, path: nil, database: nil, version_name: n
       INSTALL_DIR=/var/www/magento1/#{path}
       
       echo 'Running n98-magerun: install'
-      mr1 install -n -q --magentoVersionByName #{version_name} --installationFolder $INSTALL_DIR \
+      mr1 install -n -q --skip-root-check --magentoVersionByName #{version_name} --installationFolder $INSTALL_DIR \
           --dbHost localhost --dbUser root --dbName #{database} #{flag_sd} --baseUrl http://#{host}/#{path}
       
       echo 'Initializing software configuration'
 
       cd $INSTALL_DIR
-      mr1 -q config:set web/secure/base_url https://#{host}/#{path}/
-      mr1 -q config:set web/secure/use_in_frontend 1
-      mr1 -q config:set web/secure/use_in_adminhtml 1
-      mr1 -q cache:flush
+      mr1 -q --skip-root-check config:set web/secure/base_url https://#{host}/#{path}/
+      mr1 -q --skip-root-check config:set web/secure/use_in_frontend 1
+      mr1 -q --skip-root-check config:set web/secure/use_in_adminhtml 1
+      mr1 -q --skip-root-check cache:flush
       
       echo 'Fixing sample data CMS permissions bug'
-      mr1 db:query \"INSERT INTO permission_block (block_name, is_allowed) VALUES ('cms/block', 'is_allowed')\"
+      mr1 --skip-root-check db:query \"INSERT INTO permission_block (block_name, is_allowed) VALUES ('cms/block', 'is_allowed')\"
 
       echo 'Setting file permissions and ownership'
       
